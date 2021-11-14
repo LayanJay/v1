6a10971690c4aebbd347bc2f5875363c24f58a68
LayanJay <layanjayasinghe2@gmail.com>
2021-11-14 23:11:22 +0530

    add: footer

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className='bg-primary-base font-poppins text-white-base border-b-2 border-primary-light flex justify-center py-8'>
+      <p className='text-sm'>
+        Copyright © 2022 All Rights Reserved by Layan Jayasinghe
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
