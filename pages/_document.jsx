7e18d913feec938c22da2800bfb6659fcfd451af
LayanJay <layanjayasinghe2@gmail.com>
2021-12-14 11:33:54 +0530

    add: new favicons

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -24,9 +24,27 @@ class MyDocument extends Document {
           />
           <meta name='twitter:image' content='/logo.png' />
 
-          <link rel='icon' type='image/svg+xml' href='/favicon/favicon.svg' />
-
-          <link rel='icon' type='image/png' href='/favicon/favicon.png' />
+          <link
+            rel='apple-touch-icon'
+            sizes='144x144'
+            href='/apple-touch-icon.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='32x32'
+            href='/favicon-32x32.png'
+          />
+          <link
+            rel='icon'
+            type='image/png'
+            sizes='16x16'
+            href='/favicon-16x16.png'
+          />
+          <link rel='manifest' href='/site.webmanifest' />
+          <link rel='mask-icon' href='/safari-pinned-tab.svg' color='#5bbad5' />
+          <meta name='msapplication-TileColor' content='#da532c' />
+          <meta name='theme-color' content='#ffffff' />
         </Head>
         <body>
           <Main />
